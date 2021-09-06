82722fea55069fa31beb3c21ccb1db0fcf05b762
GiordanoRinaldi <giordanorinaldi0@gmail.com>
2021-09-06 13:43:00 +0200

    aggiunto timer 30sec

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ function numeroRandom(min, max) {
 
 
 //Un alert() espone 5 numeri generati casualmente.
+
 var numeriRandom = [];
 
 for (var i = 0; i < 5; i++){
@@ -15,5 +16,16 @@ var visioneNumeri = alert("Ricordati questi numeri " + numeriRandom);
 
 //Da li parte un timer di 30 secondi.
 //Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+
+setTimeout(
+
+    function(){
+    for (var i = 0; i < numeriRandom.length; i++){
+        prompt("Inserisci uno dei numeri");
+    }
+
+}, 2000);
+
+
 //Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
